8e2776bca1bfef103a0c3cf79cbb9bd82dae9b6c
Esteban <49877796+Esteban1007@users.noreply.github.com>
2019-06-14 17:04:16 -0500

    Delete eliminarUser.php

--- eliminarUser.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$IdUser =$_GET['idperson'];
-require "php/conexion.php";
-$Sql ="DELETE FROM usuarios WHERE id='$IdUser'";
-$Result= mysqli_query($conect,$Sql);
-header("Location:usuarios.php");
-?>
